2e1d576129dc8708c4f677189f5e91395c6bf1f6
klausleme <klausleme@proton.me>
2025-04-02 15:10:15 -0300

    Script provisionamento Apache v.1.0

---
+++ script-iacl2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Updating the server..."
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Downloading and copying the application's files...."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
